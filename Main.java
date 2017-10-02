14cdbabd780e2c1872186e86632da1f591e6f305
yoming410 <106021153@live.asia.edu.tw>
2017-10-02 16:04:34 +0800

    20171202

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	char ch = sc.next().charAt(0);
+	if (ch-'A',<= 26 && ch - 'A' >=0)
+    }
+}
